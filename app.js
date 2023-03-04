4352ffa6e51dcb25662dfe0f512234201c712f59
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-03-05 01:16:38 +0530

    fontend build

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use("/api/chat", chatRoute);
 
 // serve the static files
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/public/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 
