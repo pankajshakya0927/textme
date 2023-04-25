b8189e24e1cbbd08487a2c3571380e3a2dced6e8
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-04-25 18:33:22 +0530

    updated dependencies

--- db.js
+++ db.js
@@ -2,21 +2,12 @@ const mongoose = require("mongoose");
 const connectionUrl = "mongodb+srv://admin:6YGfGPw03iC5Mx8G@cluster0.6bcdj9j.mongodb.net/?retryWrites=true&w=majority";
 // const connectionUrl = "mongodb://127.0.0.1:27017/textme"; // local connection url
 
-mongoose.connect(
-  process.env.MONGODB_URI || connectionUrl,
-  {
+mongoose
+  .connect(process.env.MONGODB_URI || connectionUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
-  (err, db) => {
-    if (err) {
-      console.error(err);
-    }
-
-    if (db) {
-      console.log("Connected to mongodb...");
-    }
-  }
-);
+  })
+  .then((result) => console.log("Connected to mongodb..."))
+  .catch((err) => console.error(err));
 
 module.exports = mongoose;
