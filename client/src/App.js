7526cb2c94be0b31b29e8099fc485d3c29e9c426
Pankaj Shakya <pankajshakya0927@gmail.com>
2025-05-25 14:01:12 +0545

    chore: remove custom --vh viewport height fix due to unresolved mobile layout issues

--- client/src/App.js
+++ client/src/App.js
@@ -13,16 +13,6 @@ import "./App.css";
 import Homepage from "./components/Home/Homepage";
 
 function App() {
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
-    setVh();
-    window.addEventListener('resize', setVh);
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
-  
   return (
     <NotificationsContextProvider>
       <FriendsContextProvider>
