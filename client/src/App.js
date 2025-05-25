b02f6da2103e4215496e4bd6e272c59fcdb977b6
Pankaj Shakya <pankajshakya0927@gmail.com>
2025-05-25 08:37:46 +0545

    Fix mobile viewport height issue using dynamic CSS variable for --vh

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ChatTabs from "./components/ChatTabs/ChatTabs";
@@ -13,20 +13,30 @@ import "./App.css";
 import Homepage from "./components/Home/Homepage";
 
 function App() {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    setVh();
+    window.addEventListener('resize', setVh);
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
+  
   return (
     <NotificationsContextProvider>
       <FriendsContextProvider>
-      <UsersContextProvider>
-        <Router>
-          <NavbarOffCanvas />
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/chats" component={ChatTabs} />
-          </Switch>
-        </Router>
-      </UsersContextProvider>
+        <UsersContextProvider>
+          <Router>
+            <NavbarOffCanvas />
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/chats" component={ChatTabs} />
+            </Switch>
+          </Router>
+        </UsersContextProvider>
       </FriendsContextProvider>
     </NotificationsContextProvider>
   );
