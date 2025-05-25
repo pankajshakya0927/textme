09ade8d0bfba4d7530826320ceb792f82cce0941
Pankaj Shakya <pankajshakya0927@gmail.com>
2025-05-25 14:28:59 +0545

    Fix mobile layout: adjust .box height and top margin for touch devices using media queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ChatTabs from "./components/ChatTabs/ChatTabs";
